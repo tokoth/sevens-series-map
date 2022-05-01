a56ce228e35dc87deae93878489d61f8a64fe9df
Tyson Okoth <tysonokoth8@gmail.com>
2022-05-01 11:55:33 +0300

    Updated API key to google sheets

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kYzFwl1IgCd9FIaQ0IN6CJOP-EDwJiJ4P2v4AzxN2Xw/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyDyV0U5Phy9Bo5LSbdnOUqcMOGaYiJqkCo';
